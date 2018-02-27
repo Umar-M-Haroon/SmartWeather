3edb7ec65d0a9eeb4c2af0deb1413d4c95bfd606
Umar <umar.m.haroon@gmail.com>
2018-02-27 11:09:34 -0700

    First

---
+++ WeatherLamp.py
@@ -0,0 +1,19 @@
+import urllib,json
+key = "cf22c6d3079412ef13ed81f039297bc8"
+url = "http://api.openweathermap.org/data/2.5/forecast?q=Bangkok&APPID="+key+"&units=imperial"+"&cnt=4"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+print url+'\n'+'\n'
+print data["list"][0]
+# print data["main"]["temp_min"]
+# print data["main"]["temp_max"]
+# print data["clouds"]["all"]
+# print data["main"]["temp"]
+# print data["precipitation"]
+
+
+
+#main.temp_min
+#main.temp_max
+#clouds.all
+#rain.3h Rain volume for the last 3 hours
