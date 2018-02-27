7dc1557e1fa946c0a9ad8b3357c1ad76113e0c6a
Tyler Merchant <Tyler@rgnt1-10-97-dhcp.int.colorado.edu>
2018-02-27 12:32:21 -0700

    sheet

--- WeatherLamp.py
+++ WeatherLamp.py
@@ -1,13 +1,22 @@
 import urllib,json
-key = "cf22c6d3079412ef13ed81f039297bc8"
-url = "http://api.openweathermap.org/data/2.5/forecast?q=Bangkok&APPID="+key+"&units=imperial"+"&cnt=4"
-response = urllib.urlopen(url)
-data = json.loads(response.read())
 
 #main.temp_min
 #main.temp_max
 #clouds.all
 #rain.3h Rain volume for the last 3 hours
-def helloFunction():
-    # print("Hello")
-helloFunction()
+class Weather:
+    def getData(self):
+        key = "cf22c6d3079412ef13ed81f039297bc8"
+        url = "http://api.openweathermap.org/data/2.5/forecast?q=Bangkok&APPID="+key+"&units=imperial"+"&cnt=4"
+        response = urllib.urlopen(url)
+        return json.loads(response.read())
+    def filterData(self,d):
+        hour1 = d["list"][0]
+        hour2 = d["list"][1]
+        hour3 = d["list"][2]
+        hour4 = d["list"][3]
+        return [hour1,hour2,hour3,hour4]
+
+w = Weather()
+data = w.getData()
+print(w.filterData(data)[1])
