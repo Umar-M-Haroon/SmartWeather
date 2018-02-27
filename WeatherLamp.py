43f980a73a8700d925c3d47b7e971485babf7605
Umar <umar.m.haroon@gmail.com>
2018-02-27 12:52:47 -0700

    added array

--- WeatherLamp.py
+++ WeatherLamp.py
@@ -5,19 +5,26 @@ import urllib,json
 #main.temp_max
 #clouds.all
 #rain.3h Rain volume for the last 3 hours
+
+
 class Weather:
+    temp=0
+    def __init__(self,t):
+        self.temp=t
+class Data:
     def getData(self):
         key = "cf22c6d3079412ef13ed81f039297bc8"
         url = "http://api.openweathermap.org/data/2.5/forecast?q=Bangkok&APPID="+key+"&units=imperial"+"&cnt=4"
         response = urllib.urlopen(url)
         return json.loads(response.read())
     def filterData(self,d):
-        hour1 = d["list"][0]
-        hour2 = d["list"][1]
-        hour3 = d["list"][2]
-        hour4 = d["list"][3]
-        return [hour1,hour2,hour3,hour4]
+        arr = []
+        for x in data["list"]:
+            arr.append(x["main"])
+        return arr
 
-w = Weather()
-data = w.getData()
-print(w.filterData(data)[1])
+d = Data()
+data = d.getData()
+x = d.filterData(data)
+
+x =0
