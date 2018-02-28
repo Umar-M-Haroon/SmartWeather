3b7a8b17d90cb2203fbcd9d3820b38c6821c3fe4
Umar <umar.m.haroon@gmail.com>
2018-02-27 22:04:03 -0700

    Added Filter Data

--- WeatherLamp.py
+++ WeatherLamp.py
@@ -1,30 +1,41 @@
 import urllib,json
-
-
-#main.temp_min
-#main.temp_max
-#clouds.all
-#rain.3h Rain volume for the last 3 hours
-
-
 class Weather:
-    temp=0
-    def __init__(self,t):
+    temp = 0
+    rain = 0 
+    cloud = 0
+
+    def __init__(self,t,r,c):
         self.temp=t
+        self.rain=r
+        self.cloud=c
 class Data:
     def getData(self):
         key = "cf22c6d3079412ef13ed81f039297bc8"
-        url = "http://api.openweathermap.org/data/2.5/forecast?q=Bangkok&APPID="+key+"&units=imperial"+"&cnt=4"
+        url = "http://api.openweathermap.org/data/2.5/forecast?&lat=43.15&lon=-77.62&APPID="+key+"&units=imperial"+"&cnt=4"
+        print(url)
         response = urllib.urlopen(url)
         return json.loads(response.read())
     def filterData(self,d):
         arr = []
         for x in data["list"]:
-            arr.append(x["main"])
+            temp = x["main"]["temp"]
+            try:
+                rain = x["rain"]["3h"]
+            except:
+                rain=0
+            cloud = x["clouds"]["all"]
+            r = Weather(temp,rain,cloud)
+            arr.append(r)
         return arr
+
+
+
+
+
+
+
 
 d = Data()
 data = d.getData()
 x = d.filterData(data)
 
-x =0
