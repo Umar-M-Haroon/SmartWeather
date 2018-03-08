29b5bb91a234a9bd7a7cd491bfc45656f836f0ae
Umar <umar.m.haroon@gmail.com>
2018-03-07 21:53:43 -0700

    Added Blinking Red LED

--- WeatherLamp.py
+++ WeatherLamp.py
@@ -1,4 +1,6 @@
 import urllib,json
+import time
+import RPi.GPIO as GPIO
 class Weather:
     temp = 0
     rain = 0 
@@ -25,8 +27,20 @@ class Data:
     def getData(self):
         key = "cf22c6d3079412ef13ed81f039297bc8"
         url = "http://api.openweathermap.org/data/2.5/forecast?&lat=43.15&lon=-77.62&APPID="+key+"&units=imperial"+"&cnt=4"
-        response = urllib.urlopen(url)
-        return json.loads(response.read())
+        success = False
+        while (success==False):
+            try:
+                response = urllib.urlopen(url)
+                success=True
+            except:
+                time.sleep(0.5)
+                success=False
+
+        try:
+            dataResponse = json.loads(response.read())
+        except:
+            dataResponse=""
+        return dataResponse
     def filterData(self,d):
         arr = []
         for x in data["list"]:
@@ -51,14 +65,21 @@ class Data:
 
 
 d = Data()
+
 data = d.getData()
+while data is "":
+    data=d.getData()
 x = d.filterData(data)
 W = Weather(0,0,0)
 W.cloud=W.findHighestCloudPercentage(x)
 W.rain=W.findTotalRain(x)
 print(W.rain)
 print(W.cloud)
-
-
-
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(6,GPIO.OUT)
+GPIO.output(6,GPIO.HIGH)
+while True:
+        GPIO.output(6,GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(6,GPIO.LOW)
+        time.sleep(0.5)
