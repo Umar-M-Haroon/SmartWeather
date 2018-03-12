a6440eb905e1d2ad7769be733fdf0bec7b99d854
Umar <umar.m.haroon@gmail.com>
2018-03-12 17:42:46 -0600

    Removed unnecessary echo

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ echo "Choose the disk with the name 'boot': "
 read bName
 sudo dd if=$bName of=/Volumes/NO\ NAME/$s-$d.DMG
 cd /Volumes/NO\ NAME/
-echo $s-$d.DMG
+
 tar -jcvf $s-$d.tar.bz2 $s-$d.DMG
 rm $s-$d.DMG
