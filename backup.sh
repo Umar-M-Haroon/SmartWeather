c9b9804d058a04cb5430304013acf252eee70566
Umar <umar.m.haroon@gmail.com>
2018-03-12 12:38:27 -0600

    Good backup script now

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 d=`date +%m-%d`
 s="RPISW"
-sudo dd if=/dev/disk2 of=/Volumes/NO\ NAME/$s-$d.DMG
+diskutil list
+echo "Choose the disk with the name 'boot': "
+read bName
+sudo dd if=$bName of=/Volumes/NO\ NAME/$s-$d.DMG
 cd /Volumes/NO\ NAME/
 echo $s-$d.DMG
 tar -jcvf $s-$d.tar.bz2 $s-$d.DMG
