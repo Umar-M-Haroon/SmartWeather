26de7bbe71ee06755561f1ad6cfdff30c15a7822
Umar <umar.m.haroon@gmail.com>
2018-03-15 20:46:34 -0600

    Added Lightning Funciton

--- WeatherLamp.py
+++ WeatherLamp.py
@@ -3,6 +3,32 @@ import RPi.GPIO as GPIO
 import time
 from  neopixel import *
 import sys
+
+class Board:
+# LED strip configuration:
+    LED_COUNT      = 16      # Number of LED pixels.
+    LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+    LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+    LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+    LED_BRIGHTNESS = 25     # Set to 0 for darkest and 255 for brightest
+    LED_INVERT     = False   # True to invert the signal (when using NPN transistor$
+    LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+    LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
+    ring = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP) 
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        self.ring.begin()
+    def setPins(self,pins):
+        for i in pins:
+            if pins[i] is "OUT":
+                GPIO.setup(i,GPIO.OUT)
+            else:
+                GPIO.setup(i,GPIO.IN)
+    def setColor(self,r,g,b,LEDNumbers):
+        for i in LEDNumbers:
+            self.ring.setPixelColor(i,r,g,b)
+
+
 class Weather:
     temp = 0
     rain = 0
@@ -22,12 +48,15 @@ class Weather:
             if (x.cloud > max):
                 max=x.cloud
         return max
-
+    def makeLightning(self,rain):
+        if rain >= 2:
+            b=Board()
+            b.setColor(139,0,139,[3,7,12,15])
 
 class Data:
     def getData(self):
         key = "cf22c6d3079412ef13ed81f039297bc8"
-        url = "http://api.openweathermap.org/data/2.5/forecast?&lat=43.15&lon=-$
+        url = "http://api.openweathermap.org/data/2.5/forecast?&lat=43.15&lon=-77.62&APPID="+key+"&units=imperial"+"&cnt=4"
         success = False
         while (success==False):
             try:
@@ -57,15 +86,6 @@ class Data:
         return arr
 
 
-
-
-
-
-
-
-
-
-
 d = Data()
 data = d.getData()
 while data is "":
@@ -77,36 +97,9 @@ W.rain=W.findTotalRain(x)
 print(W.rain)
 print(W.cloud)
 
-
-
-# LED strip configuration:
-LED_COUNT      = 16      # Number of LED pixels.
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-#LED_PIN        = 10      # GPIO pin connected to the pixels (10 uses SPI /dev/$
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 25     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor$
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
-LED_STRIP      = ws.WS2811_STRIP_GRB   # Strip type and colour ordering
-
-
-
-
-
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(6,GPIO.OUT)
-GPIO.output(6,GPIO.HIGH)
-ring = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL, LED_STRIP) 
-ring.begin()
-
-def theaterChase(strip, color, wait_ms=50, iterations=10):
-        """Movie theater light style chaser animation."""
-        for j in range(iterations):
-                for q in range(3):
-                        for i in range(0, strip.numPixels(), 3):
-                                strip.setPixelColor(i+q, 0)
+b=Board()
+pins = {18:"OUT"}
+b.setPins(pins)
 
 try:
         while True:
@@ -114,9 +107,7 @@ try:
                 time.sleep(0.5)
                 GPIO.output(6,GPIO.LOW)
                 time.sleep(0.5)
-                theaterChase(ring, Color(127, 127, 127))  # White theater chase
-                theaterChase(ring, Color(127,   0,   0))  # Red theater chase
-                theaterChase(ring, Color(  0,   0, 127))  # Blue theater chase
+                W.makeLightning(2) 
 except KeyboardInterrupt:
         print("INTERRUPT")
 finally:
