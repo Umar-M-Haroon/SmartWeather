5542397f4e6a4548d70ed41a8bc353794bfd03a0
Umar <umar.m.haroon@gmail.com>
2018-03-20 11:29:57 -0600

    ratio

--- WeatherLamp.py
+++ WeatherLamp.py
@@ -81,7 +81,7 @@ class Weather:
         LEDs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
         minimum = -10
         maximum = 100
-        ratio = 2*(temp-minimum)/(maximum - minimum)
+        ratio = ((temp-minimum)/(maximum - minimum))/2
         print(ratio)
         blue = int(max(0,255*(1-ratio)))
         red = int(max(0,255*(ratio - 1)))
