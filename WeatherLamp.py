9d056209d9508687499329bac7d16e4e47474430
Umar <umar.m.haroon@gmail.com>
2018-03-22 11:27:45 -0600

    nr

--- WeatherLamp.py
+++ WeatherLamp.py
@@ -153,7 +153,7 @@ b.setPins(pins)
 
 try:
     while True:
-        c = W.makeTemp(40)
+        W.makeTemp(40)
         # print(c)
         # time.sleep(1)
         # W.makeLightning(20,c[0],c[1],c[2])
