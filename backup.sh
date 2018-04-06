49e9cdc8c18bfbf15e0513203256c53e3c7d7e21
Umar <umar.m.haroon@gmail.com>
2018-04-06 14:16:51 -0600

    comments for scripts

--- backup.sh
+++ backup.sh
@@ -1,10 +1,14 @@
+#get the current date and create a string RPISW
 d=`date +%m-%d`
 s="RPISW"
+#Show connected disks and asks for the
+#user to choose the SD card disk and stores the disk name in bName
 diskutil list
 echo "Choose the disk with the name 'boot': "
 read bName
+#create the .dmg from the SD card and output it to the USB
 sudo dd if=$bName of=/Volumes/NO\ NAME/$s-$d.DMG
 cd /Volumes/NO\ NAME/
-
+#Go to the USB, compress the backup, delete the original DMG to save space
 tar -jcvf $s-$d.tar.bz2 $s-$d.DMG
 rm $s-$d.DMG
--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,9 @@
+# Navigates to the SmartWeather directory
 cd /
 cd /home/pi/Desktop
 cd SmartWeather
+#Pulls from the github repository, runs the code
+#and sets the current directory to /
 sudo git pull
 sudo python WeatherLamp.py
 cd /
