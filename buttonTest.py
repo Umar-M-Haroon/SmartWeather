4be2ef965bdaf25c03ebdb51a038db704570c93f
Umar <umar.m.haroon@gmail.com>
2018-04-11 17:26:03 -0600

    added tester for button

---
+++ buttonTest.py
@@ -0,0 +1,14 @@
+import RPi.GPIO as GPIO
+import time
+import sys
+import random
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(12,GPIO.OUT)
+
+while true:
+    x = GPIO.input(12)
+    if x:
+        print("HELLO")
+        x = false
