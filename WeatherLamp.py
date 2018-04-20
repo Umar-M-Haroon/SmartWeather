c79ea5af45b80fa61cc3f4d50231e2257b0ae55a
Umar <umar.m.haroon@gmail.com>
2018-04-20 12:03:28 -0600

    temp change

--- WeatherLamp.py
+++ WeatherLamp.py
@@ -187,15 +187,15 @@ def mainLoop():
         # time.sleep(1)
 def secondLoop():
     while True:
-        W.makeLightning(4,W.makeTemp(40)[0],W.makeTemp(40)[1],W.makeTemp(40)[2])
+        W.makeLightning(4,W.makeTemp(90)[0],W.makeTemp(90)[1],W.makeTemp(90)[2])
         time.sleep(random.random())
 if __name__ == '__main__':
     try:
-        t = W.makeTemp(40)
+        t = W.makeTemp(90)
         Process(target=mainLoop).start()
         Process(target=secondLoop).start()
         while True:
-            t = W.makeTemp(40)
+            t = W.makeTemp(90)
     except KeyboardInterrupt:
         print("INTERRUPTED")
     finally:
