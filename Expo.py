67bd96d2f35c3040e83939e39d2445804b98ab76
Tyler Merchant <Tyler@res1-166-172-dhcp.int.colorado.edu>
2018-04-27 21:03:03 -0600

    sdfa3a

--- Expo.py
+++ Expo.py
@@ -151,7 +151,7 @@ try:
                 for _ in range(15):
                     temp = Wi.makeTemp(Wi.temp)
                     for j in range(20):
-                        Wi.makeLightning(4,temp[0],temp[1],temp[2])
+                        Wi.makeLightning(4,temp[0],temp[1],temp[2],temp[3])
                         time.sleep(random.random())
                     time.sleep(1) 
 except KeyboardInterrupt:
